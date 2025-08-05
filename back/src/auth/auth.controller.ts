c9e096feb8fcebc46a0b87ceeaf5641d7c576bd7
Yann <yann.duchateau97@gmail.com>
2025-08-05 09:39:39 +0200

    correction codeHTTP 201->200

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
@@ -13,6 +13,7 @@ export class AuthController {
   }
 
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   async login(@Body(ValidationPipe) loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
